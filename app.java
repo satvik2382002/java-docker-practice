2b9d70bb6d1618bbd3805ec32c0f96dd0da0188c
Satvik <thakorsatvik86@gmail.com>
2026-02-22 16:09:09 +0100

    Initial Java project

---
+++ app.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Docker Practice Project");
+        System.out.println("Current User: " + System.getProperty("user.name"));
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+        System.out.println("Project Running Successfully 🚀");
+    }
+}
